52bdf9e01ca94e0fa42a3817842eee443fe0fc1a
YoshitakaKato <yoshitaka.kato@l-is-b.com>
2025-10-15 15:18:31 +0900

    kspProcess を作成し KSClassDeclarationExtTest で使用する ksp の処理を共通化
    `testGetAllParentFiles`, `testGetFieldsWithStaticField`, `testGetFieldsWithOutStaticField` で使用する共通処理を `kspProcess` として抜き出し共通化した

--- compiler/src/test/java/com/lisb/google/devtools/ksp/symbol/KSClassDeclarationExtTest.kt
+++ compiler/src/test/java/com/lisb/google/devtools/ksp/symbol/KSClassDeclarationExtTest.kt
@@ -1,6 +1,5 @@
 package com.lisb.google.devtools.ksp.symbol
 
-import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
@@ -9,6 +8,7 @@ import com.google.devtools.ksp.symbol.KSAnnotated
 import com.lisb.google.devtools.ksp.symbol.KSClassDeclarationExt.getAllParentFiles
 import com.lisb.google.devtools.ksp.symbol.KSClassDeclarationExt.getFields
 import com.lisb.java.io.File.FileExt.toSourceFile
+import com.tschuchort.compiletesting.JvmCompilationResult
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.configureKsp
 import junit.framework.Assert.assertEquals
@@ -20,121 +20,103 @@ class KSClassDeclarationExtTest {
     @OptIn(ExperimentalCompilerApi::class)
     @Test
     fun testGetAllParentFiles() {
-        val result = KotlinCompilation().apply {
-            configureKsp(useKsp2 = true) {
-                symbolProcessorProviders += TestGetAllParentFilesProvider()
-            }
-            sources = listOf(
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource1.kt").toSourceFile(),
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource1Parent.kt").toSourceFile(),
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource1GrandParent.kt").toSourceFile(),
+        val result = kspProcess(
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource1.kt",
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource1Parent.kt",
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource1GrandParent.kt"
+        ) { resolver ->
+            val name =
+                resolver.getKSNameFromString("com.lisb.google.devtools.ksp.symbol.TestSource1")
+            val parentFiles = resolver.getClassDeclarationByName(name)!!.getAllParentFiles()
+            assertEquals(
+                parentFiles.map { it.declarations.first().qualifiedName!!.asString() }
+                    .sorted(),
+                listOf(
+                    "com.lisb.google.devtools.ksp.symbol.TestSource1Parent",
+                    "com.lisb.google.devtools.ksp.symbol.TestSource1GrandParent"
+                ).sorted()
             )
-        }.compile()
+        }
         assertEquals(result.messages, result.exitCode, KotlinCompilation.ExitCode.OK)
-    }
-
-    private class TestGetAllParentFilesProvider : SymbolProcessorProvider {
-        override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
-            return Processor()
-        }
-
-        private class Processor() : SymbolProcessor {
-
-            @OptIn(KspExperimental::class)
-            override fun process(resolver: Resolver): List<KSAnnotated> {
-                val name =
-                    resolver.getKSNameFromString("com.lisb.google.devtools.ksp.symbol.TestSource1")
-                val parentFiles = resolver.getClassDeclarationByName(name)!!.getAllParentFiles()
-                assertEquals(
-                    parentFiles.map { it.declarations.first().qualifiedName!!.asString() }
-                        .sorted(),
-                    listOf(
-                        "com.lisb.google.devtools.ksp.symbol.TestSource1Parent",
-                        "com.lisb.google.devtools.ksp.symbol.TestSource1GrandParent"
-                    ).sorted()
-                )
-                return emptyList()
-            }
-        }
     }
 
     @OptIn(ExperimentalCompilerApi::class)
     @Test
     fun testGetFieldsWithStaticField() {
-        val result = KotlinCompilation().apply {
-            configureKsp(useKsp2 = true) {
-                symbolProcessorProviders += TestGetFieldsProvider(withStaticField = true) { fieldNames ->
-                    assertEquals(
-                        fieldNames,
-                        listOf(
-                            "PrivateTestSource",
-                            "PublicTestSource",
-                            "ProtectedTestCaseParent",
-                            "PublicTestCaseGrandParent",
-                            "STATIC_FIELD",
-                            "PROTECTED_PARENT_STATIC_FIELD",
-                            "PUBLIC_GRAND_PARENT_STATIC_FIELD"
-                        ).sorted()
-                    )
-                }
-            }
-            sources = listOf(
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2.java").toSourceFile(),
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2Parent.java").toSourceFile(),
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2GrandParent.java").toSourceFile(),
+        val result = kspProcess(
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2.java",
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2Parent.java",
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2GrandParent.java"
+        ) { resolver ->
+            val name =
+                resolver.getKSNameFromString("com.lisb.google.devtools.ksp.symbol.TestSource2")
+            val fields = resolver.getClassDeclarationByName(name)!!
+                .getFields(withStaticField = true)
+                .toList()
+            assertEquals(
+                fields.map { it.simpleName.asString() }.sorted(),
+                listOf(
+                    "PrivateTestSource",
+                    "PublicTestSource",
+                    "ProtectedTestCaseParent",
+                    "PublicTestCaseGrandParent",
+                    "STATIC_FIELD",
+                    "PROTECTED_PARENT_STATIC_FIELD",
+                    "PUBLIC_GRAND_PARENT_STATIC_FIELD"
+                ).sorted()
             )
-        }.compile()
+        }
         assertEquals(result.messages, result.exitCode, KotlinCompilation.ExitCode.OK)
     }
 
     @OptIn(ExperimentalCompilerApi::class)
     @Test
     fun testGetFieldsWithOutStaticField() {
-        val result = KotlinCompilation().apply {
-            configureKsp(useKsp2 = true) {
-                symbolProcessorProviders += TestGetFieldsProvider(withStaticField = false) { fieldNames ->
-                    assertEquals(
-                        fieldNames,
-                        listOf(
-                            "PrivateTestSource",
-                            "PublicTestSource",
-                            "ProtectedTestCaseParent",
-                            "PublicTestCaseGrandParent",
-                        ).sorted()
-                    )
-                }
-            }
-            sources = listOf(
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2.java").toSourceFile(),
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2Parent.java").toSourceFile(),
-                File("src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2GrandParent.java").toSourceFile(),
+        val result = kspProcess(
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2.java",
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2Parent.java",
+            "src/test/java/com/lisb/google/devtools/ksp/symbol/TestSource2GrandParent.java"
+        ) { resolver ->
+            val name =
+                resolver.getKSNameFromString("com.lisb.google.devtools.ksp.symbol.TestSource2")
+            val fields = resolver.getClassDeclarationByName(name)!!
+                .getFields(withStaticField = false)
+                .toList()
+            assertEquals(
+                fields.map { it.simpleName.asString() }.sorted(),
+                listOf(
+                    "PrivateTestSource",
+                    "PublicTestSource",
+                    "ProtectedTestCaseParent",
+                    "PublicTestCaseGrandParent"
+                ).sorted()
             )
-        }.compile()
+        }
         assertEquals(result.messages, result.exitCode, KotlinCompilation.ExitCode.OK)
     }
 
-    private class TestGetFieldsProvider(
-        private val withStaticField: Boolean,
-        private val assert: (List<String>) -> Unit
-    ) : SymbolProcessorProvider {
-        override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
-            return Processor()
-        }
-
-        private inner class Processor() : SymbolProcessor {
-
-            @OptIn(KspExperimental::class)
-            override fun process(resolver: Resolver): List<KSAnnotated> {
-                val name =
-                    resolver.getKSNameFromString("com.lisb.google.devtools.ksp.symbol.TestSource2")
-                val fields =
-                    resolver.getClassDeclarationByName(name)!!
-                        .getFields(withStaticField = withStaticField)
-                        .toList()
-                this@TestGetFieldsProvider.assert.invoke(fields.map { it.simpleName.asString() }
-                    .sorted())
-                return emptyList()
+    /**
+     * [sources] をコンパイルした際に実行されるKSPの [SymbolProcessor.process] 内で [block] を実行する
+     */
+    @OptIn(ExperimentalCompilerApi::class)
+    private fun kspProcess(
+        vararg sources: String,
+        block: (Resolver) -> Unit
+    ): JvmCompilationResult {
+        return KotlinCompilation().apply {
+            configureKsp(useKsp2 = true) {
+                symbolProcessorProviders += object : SymbolProcessorProvider {
+                    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+                        return object : SymbolProcessor {
+                            override fun process(resolver: Resolver): List<KSAnnotated> {
+                                block(resolver)
+                                return emptyList()
+                            }
+                        }
+                    }
+                }
             }
-        }
+            this.sources = sources.map { File(it).toSourceFile() }
+        }.compile()
     }
 }
